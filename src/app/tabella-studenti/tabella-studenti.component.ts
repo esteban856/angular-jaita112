19d6147a6e4a8e65ff856da7550a969b8e8e6769
esteban.cpp <stefano.iannone9603@gmail.com>
2024-03-14 12:47:11 +0100

    Eliminazione studente implementata, fine giornata 14-03-24

--- src/app/tabella-studenti/tabella-studenti.component.ts
+++ src/app/tabella-studenti/tabella-studenti.component.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Component, Input } from '@angular/core';
 import { Studente } from 'src/models/Studente';
 
@@ -13,5 +13,36 @@ export class TabellaStudentiComponent {
 
   constructor(private http : HttpClient){
     this.http = http;
+  }
+
+
+  eliminaStudente(id : number){
+    var token = sessionStorage.getItem("token")
+
+    if(token == null){
+      token = "";
+    }
+
+    const headers = new HttpHeaders(
+      {
+        'Content-Type' : 'application/json',
+        'token': token as string
+      }
+    );
+
+    const params = new HttpParams().set("idStudente", id);
+
+    this.http.get("http://localhost:8080/api/persona/studente-delete", {headers, params}).subscribe(risposta =>{
+      var check = risposta as boolean;
+      if(check){
+        alert("Eliminazione avvenuta con successo")
+        // window.location.href = "/areadirigenti"
+        var indice = this.studenti?.findIndex(x => x.id === id)
+        console.log("indice", indice)
+        if(indice! > -1){
+          this.studenti?.splice(indice!, 1);
+        }
+      }
+    })
   }
 }
